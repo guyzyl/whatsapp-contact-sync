eb2936159a957ecd2425ec9a718a3a41b2bb8ebc
Guy Zylberberg <guyzyl@gmail.com>
2022-08-07 11:17:56 +0300

    Update entrypoint.sh

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+npm run prod &
+
+# Wait for the server to start
+while ! nc -z localhost 8080; do
+  sleep 0.1
+done
+
 nginx &
-npm run prod
+
+# Crach container if node failes
+wait %1
