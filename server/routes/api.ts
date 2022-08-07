cd58e42c30d0cadc8112e0dc02d5faae603311ce
Guy Zylberberg <guyzyl@gmail.com>
2022-08-07 17:45:16 +0300

    Split routes from `server.ts`
    Also renamed `server.ts` to `main.ts`

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -1,88 +1,20 @@
-import cors from "cors";
 import express from "express";
 import { Request, Response } from "express";
-import expressWs from "express-ws";
 import { WebSocket } from "ws";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import session from "express-session";
+// @ts-ignore
+import patch from "express-ws/lib/add-ws-method";
 
 import { Auth } from "googleapis";
 import { Client, WAState } from "whatsapp-web.js";
 
-import winston from "winston";
-import expressWinston from "express-winston";
-
 import { SessionStatus } from "../../interfaces/api";
-import { initWhatsApp } from "./whatsapp";
-import { initSync } from "./sync";
-import { googleLogin } from "./gapi";
+import { initWhatsApp } from "../src/whatsapp";
+import { initSync } from "../src/sync";
+import { googleLogin } from "../src/gapi";
 
-import dotenv from "dotenv";
-dotenv.config();
-
-let ews = expressWs(express());
-const app = ews.app;
-let router = express.Router({ mergeParams: true });
-const port = 8080;
-
-/*
-  Setup the session and cookie parser.
-  Based on - https://stackoverflow.com/a/55597997/1403643
-*/
-app.use(
-  cors({
-    origin: [process.env.ORIGIN || "http://localhost:8080"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // enable set cookie
-  })
-);
-
-const secret = process.env.SESSION_SECRET || "secret";
-app.use(bodyParser.json());
-app.use(cookieParser(secret));
-app.use(
-  session({
-    secret: secret,
-    cookie: {
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours
-      httpOnly: false,
-      secure: process.env.NODE_ENV == "production" ? true : false,
-    },
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-app.use(function (req: Request, res: Response, next: CallableFunction) {
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-   Type, Accept, Authorization"
-  );
-  next();
-});
-
-if (process.env.NODE_ENV == "production") {
-  app.set("trust proxy", 1);
-}
-
-app.use(
-  expressWinston.logger({
-    transports: [new winston.transports.Console()],
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.json()
-    ),
-    expressFormat: true,
-    colorize: false,
-  })
-);
-
-// To fix 304 responses.
-app.disable("etag");
+// Based on https://github.com/HenningM/express-ws/issues/86
+patch(express.Router);
+const router = express.Router({ mergeParams: true });
 
 /*
   Session id to objects mapping (since they cant be stored in session directly).
@@ -115,10 +47,6 @@ function cleanup(sessionID: string) {
 
   cleanupMap[sessionID] = timeout;
 }
-
-/*
-  Setup the routes.
-*/
 
 router.ws("/ws", (ws: WebSocket, req: Request) => {
   if (cleanupMap[req.sessionID] !== undefined) {
@@ -171,8 +99,4 @@ router.get("/init_sync", (req: Request, res: Response) => {
   res.send("{}");
 });
 
-const routePrefix = process.env.ROUTE_PREFIX || "";
-app.use(routePrefix, router);
-app.listen(port, "localhost", () => {
-  console.log(`Listening on port ${port}`);
-});
+export default router;
