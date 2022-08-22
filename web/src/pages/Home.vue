8386d67817d3b0b39fc3243d7308c25b3cef82a1
Guy Zylberberg <guyzyl@gmail.com>
2022-08-22 14:42:58 +0300

    Update Home.vue
    Disable "Get Started" button until the WS is connected.

--- web/src/pages/Home.vue
+++ web/src/pages/Home.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { WS } from "../services/ws";
+
+export default defineComponent({
+  data: () => ({
+    WS: WS,
+    WebSocket: WebSocket,
+  }),
+  mounted() {},
+  methods: {},
+});
+</script>
 
 <template>
   <div id="home" class="hero h-full bg-base-200">
@@ -19,8 +31,13 @@
             >GitHub repository</a
           >.
         </p>
-        <router-link to="/whatsapp" tag="button" class="btn btn-primary"
-          >Get Started</router-link
+        <!-- Button is disabled until WS is connected to ensure the backed is serving before continuing. -->
+        <router-link
+          to="/whatsapp"
+          class="btn btn-primary"
+          :class="{ 'btn-disabled': WS.readyState !== WebSocket.OPEN }"
+        >
+          Get Started</router-link
         >
       </div>
     </div>
