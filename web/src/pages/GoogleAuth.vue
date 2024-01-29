260622df08ebe112980b5d7d3d9096d1df484c2d
Guy Zylberberg <guyzyl@gmail.com>
2024-01-29 23:09:39 +0200

    Replaced Google login button logo file

--- web/src/pages/GoogleAuth.vue
+++ web/src/pages/GoogleAuth.vue
@@ -97,7 +97,7 @@ export default defineComponent({
             <img
               class="w-8"
               alt="Google login"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+              src="/google_logo.svg"
             />
             Sign in with Google
           </button>
