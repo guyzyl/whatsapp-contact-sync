7d119cf40aa94237a4a6cb07f70d5d039586b323
Guy Zylberberg <guyzyl@gmail.com>
2024-03-18 13:41:22 +0200

    Update GitHub button width in Header.vue

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -21,7 +21,7 @@ export default defineComponent({
         src="https://ghbtns.com/github-btn.html?user=guyzyl&repo=whatsapp-contact-sync&type=star&count=true&size=large"
         frameborder="0"
         scrolling="0"
-        width="130"
+        width="140"
         height="30"
         title="GitHub"
       ></iframe>
