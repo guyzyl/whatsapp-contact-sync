75b6297a021e67c6c10395480a9a51f98d190c44
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-11-21 20:37:04 +1100

    feat: :hammer: make version bumping interactive

--- bump-version.sh
+++ bump-version.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
 
 # Usage: ./bump-version.sh [patch|minor|major]
-# Default is patch if no argument provided
 
-VERSION_TYPE=${1:-patch}
+if [[ -z "$1" ]]; then
+	echo "Which version to bump?"
+	echo "1) patch (x.y.Z) [default]"
+	echo "2) minor (x.Y.0)"
+	echo "3) major (X.0.0)"
+
+	# Countdown timer
+	for i in {5..1}; do
+		echo -ne "\rEnter choice [1-3] (timeout in ${i}s, default=1): "
+		read -r -t 1 -n 1 choice && break
+	done
+
+	if [[ -z "${choice}" ]]; then
+		echo ""
+		echo "Timeout reached. Using default: patch"
+		choice=1
+	else
+		echo ""
+	fi
+
+	case ${choice} in
+	1 | "") VERSION_TYPE="patch" ;;
+	2) VERSION_TYPE="minor" ;;
+	3) VERSION_TYPE="major" ;;
+	*)
+		echo "Invalid choice. Using default: patch"
+		VERSION_TYPE="patch"
+		;;
+	esac
+else
+	VERSION_TYPE=$1
+fi
 
 echo "Bumping ${VERSION_TYPE} version for web and server..."
 
 # Bump web version
-cd web && npm version "${VERSION_TYPE}" --no-git-tag-version
-cd ..
+(cd web && npm version "${VERSION_TYPE}" --no-git-tag-version)
 
 # Bump server version
-cd server && npm version "${VERSION_TYPE}" --no-git-tag-version
-cd ..
+(cd server && npm version "${VERSION_TYPE}" --no-git-tag-version)
 
 WEB_VERSION=$(node -p "require('./web/package.json').version")
 SERVER_VERSION=$(node -p "require('./server/package.json').version")
